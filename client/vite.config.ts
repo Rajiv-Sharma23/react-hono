6c602a51f0d64c56a452bd23710c3d2a4ee265a8
Rajiv-Sharma23 <rajivsharma0246@gmail.com>
2026-06-08 17:37:28 +0530

    first

---
+++ client/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+      
+    },
+  },
+})
+
+export default config
